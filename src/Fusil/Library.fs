ab9f041a7f0e648e3540f7299dbdc42f596a4ae8
Gaël Bouquain <gael.bouquain@gmail.com>
2025-05-06 11:42:00 +0200

    Copy fzf implementation

--- src/Fusil/Library.fs
+++ src/Fusil/Library.fs
@@ -8,6 +8,8 @@ open Shared.Char
 open Shared.CharNormalization
 open Shared.Array2D
 open Shared.Slab
+open Shared
+open System.Collections.Generic
 
 /// Contains bonuses and penalties (negative bonus)
 module private Bonus = // Copied from fzf code
@@ -86,209 +88,294 @@ module private Bonus = // Copied from fzf code
             (fun i j -> bonusFor (enum<CharClass> i) (enum<CharClass> j))
 
 #if DEBUG && !FABLE_COMPILER
-let private debug (scoreMatrix: Array2D<int16>) (query: char array) (candidate: char array) =
-    printf "         "
-    candidate |> Seq.iter (printf "%c    ")
-    printfn ""
+// let private debug (scoreMatrix: Array2D<int16>) (query: char array) (candidate: char array) =
+//     printf "         "
+//     candidate |> Seq.iter (printf "%c    ")
+//     printfn ""
 
-    for i = 0 to scoreMatrix.Height - 1 do
-        printf "%c " query[i]
+//     for i = 0 to scoreMatrix.Height - 1 do
+//         printf "%c " query[i]
 
-        for j = 0 to scoreMatrix.Width - 1 do
-            let s = scoreMatrix[i, j]
-            if s <> 0s then
-                Console.ForegroundColor <- ConsoleColor.Yellow
-            printf "%s  " <| s.ToString "000"
-            if s <> 0s then
-                Console.ForegroundColor <- ConsoleColor.White
+//         for j = 0 to scoreMatrix.Width - 1 do
+//             let s = scoreMatrix[i, j]
+//             if s <> 0s then
+//                 Console.ForegroundColor <- ConsoleColor.Yellow
+//             printf "%s  " <| s.ToString "000"
+//             if s <> 0s then
+//                 Console.ForegroundColor <- ConsoleColor.White
+//         printfn ""
+let private debug (T: int Collections.Generic.IList) (pattern: char array) (F: int32 Collections.Generic.IList) (lastIdx: int) (H: int16 Collections.Generic.IList) (C: int16 Collections.Generic.IList) =
+    let width = lastIdx - int F[0] + 1
+
+    for i in 0 .. pattern.Length - 1 do
+        let f = int F[i]
+        let I = i * width
+
+        if i = 0 then
+            printf "  "
+            for j in f .. lastIdx do
+                printf " %c " (char T[j])
+            printfn ""
+
+        printf "%c " pattern[i]
+
+        for idx in int F[0] .. f - 1 do
+            printf " 0 "
+
+        for idx in f .. lastIdx do
+            let h = H[i * width + idx - int F[0]]
+            printf "%2d " h
+        printfn ""
+
+        printf "  "
+        for idx = 0 to width - 1 do
+            let col = idx + int F[0]
+            let p =
+                if col < int F[i] then 0s
+                else C[I + idx]
+
+            if p > 0s then
+                printf "%2d " p
+            else
+                printf "   "
         printfn ""
 #endif
 
-let fuzzyMatch
-    (slab: Slab) // Arrays used to store computation data
-    caseSensitive // Does case matter
-    normalize // Transform accentuated characters to ascii
-    (queryStr: string)
-    (candidateStr: string)
-    =
-    let query = queryStr.ToCharArray()
-    let candidate = candidateStr.ToCharArray()
+let posSet withPos (length: int) =
+    if withPos then
+        HashSet length
+    else
+        null
 
-    /// Query length
-    let m = query.Length
-    /// Candidate length
-    let n = candidate.Length
-    if m > n then
+module GoSimilar =
+    let copyRunes (arr: int32 Collections.Generic.IList) (str: string) =
+        let mutable enumerator = str.EnumerateRunes()
+        let mutable i = 0
+        while enumerator.MoveNext() do
+            arr[i] <- enumerator.Current.Value
+            i <- i+1
+
+let fuzzyMatch caseSensitive normalize withPos (slab: Slab) (pattern: char array) (input: string) =
+    // Assume that pattern is given in lowercase if case-insensitive.
+    // First check if there's a match and calculate bonus for each position.
+    // If the input string is too long, consider finding the matching chars in
+    // this phase as well (non-optimal alignment).
+    let M = pattern.Length
+    if M = 0 then
+        Some (0, 0, 0s, null) // TODO: Use a struct
+    else
+
+    let N = input.Length
+    if M > N then
         None
     else
 
-    if m = 0 then
-        None
+    // Since O(nm) algorithm can be prohibitively expensive for large input,
+    // we fall back to the greedy algorithm.
+    if N*M > slab.i16.Length then
+        None // Todo: fallback on a faster algorithm
     else
 
-    // Phase 1:
-    //  Assign bonus to each character of the candidate
-    //  Check if all query characters exist in the candidate
-    //  Find the first appearance index of each query character in the candidate
-    //  Find the last matching character index
+    // Todo: build phase 1
+    // Phase 1. Optimized search for ASCII string
+    let minIdx, maxIdx = 0, input.Length
 
-    // This is the offset used to allocate the different arrays in the slab
-    let slabOffset = 0
+    // Reuse pre-allocated integer slice to avoid unnecessary sweeping of garbages
+    let mutable offset16 = 0
+    let mutable offset32 = 0
+    let mutable offset16, H0 = slab |> Slab.alloc16 offset16 N
+    let mutable offset16, C0 = slab |> Slab.alloc16 offset16 N
+    // Bonus point for each position
+    let mutable offset16, B = slab |> Slab.alloc16 offset16 N
+    // The first occurrence of each character in the pattern
+    let mutable offset32, F = slab |> Slab.alloc32 offset32 M
+    // Rune array
+    let mutable _, T = slab |> Slab.alloc32 offset32 N
+    input |> GoSimilar.copyRunes T
 
-    let mutable slabOffset, bonuses = slab |> Slab.alloc16 slabOffset n
-    let mutable slabOffset, firstCharOccurrence = slab |> Slab.alloc16 slabOffset m
-    let mutable lastMatchingIndex = 0
+    // Phase 2: Calculate bonus for each point
+    let mutable maxScore, maxScorePos = 0s, 0
+    let mutable pidx, lastIdx = 0, 0
+    let mutable pchar0, pchar, prevH0, prevClass, inGap = pattern[0], pattern[0], 0s, CharClass.initialCharClass, false
 
-    let slabOffset, scoreMatrix = slab |> Array2D.allocFromSlab m (n + 1) slabOffset
-    let _slabOffset, consecutiveMatrix = slab |> Array2D.allocFromSlab m (n + 1) slabOffset
-    scoreMatrix[0, 0] <- 0s // Needed for correct backtracking
-
-    let mutable bestScore = 0s
-    let mutable bestPos = 0, 0
-    let mutable inGap = false
-
-    let mutable prevCharClass = CharClass.initialCharClass
-    let mutable queryCharIdx = 0
-    let mutable prevCharScore = 0s
-    let mutable i = 0
-    while i <= n-1 && queryCharIdx <= m do
-        let mutable charClass = CharClass.CharWhite
-        if candidate[i] |> Char.isAscii then
-            charClass <- candidate[i] |> CharClass.ofAscii
-
-            if not caseSensitive && charClass = CharClass.CharUpper then
-                candidate[i] <- candidate[i] + char 32
+    for off = 0 to N-1 do
+        let mutable char' = T[off]
+        let mutable class' = CharClass.CharWhite
+        if char' |> char |> Char.isAscii then // TODO: Convert char functions to runes
+            class' <- CharClass.asciiCharClasses[char']
+            if not caseSensitive && class' = CharClass.CharUpper then
+                char' <- char' + 32
+                T[off] <- char'
         else
-            charClass <- candidate[i] |> CharClass.ofNonAscii
-
-            if not caseSensitive && charClass = CharClass.CharUpper then
-                candidate[i] <- candidate[i] |> Char.ToLower
+            class' <- char' |> char |> CharClass.ofNonAscii
+            if not caseSensitive && class' = CharClass.CharUpper then
+                char' <- char' |> char |> Char.ToLower |> int
 
             if normalize then
-                candidate[i] <- candidate[i] |> Char.normalize
+                char' <- CharNormalization.Char.normalize (char' |> char) |> int
 
-        let bonus = Bonus.matrix[int prevCharClass, int charClass]
-        bonuses[i] <- bonus
+            T[off] <- char'
 
-        if queryCharIdx < m && query[queryCharIdx] = candidate[i] then // Characters match
-            // Update last matching index
-            lastMatchingIndex <- i
+        let bonus = Bonus.matrix[int prevClass, int class']
+        B[off] <- bonus
+        prevClass <- class'
 
-            if queryCharIdx = 0 then
-                bonuses[i] <- bonus * Bonus.firstCharMultiplier
+        if char char' = pchar then
+            if pidx < M then
+                F[pidx] <- off
+                pidx <- pidx+1
+                pchar <- pattern[min pidx (M-1)]
+            lastIdx <- off
 
-            // Set first occurrence
-            firstCharOccurrence[queryCharIdx] <- int16 i
-            queryCharIdx <- queryCharIdx+1
-
-        // Set score if match
-        if candidate[i] = query[0] then
+        if char char' = pchar0 then
             let score = Bonus.scoreMatch + bonus*Bonus.firstCharMultiplier
-            scoreMatrix[0, i+1] <- score
-            consecutiveMatrix[0, i+1] <- 1s
+            H0[off] <- score
+            C0[off] <- 1s
+
+            if M = 1 && score > maxScore then
+                maxScore <- score
+                maxScorePos <- off
+                if bonus >= Bonus.boundary then
+                    // break // TODO: Break...
+                    ()
+
             inGap <- false
-            prevCharScore <- score
-
-            if score >= bestScore then
-                bestScore <- score
-                bestPos <- 0, i+1
         else
-            let gapPenalty =
-                match inGap with
-                | false -> Bonus.scoreGapStart
-                | true -> Bonus.scoreGapExtension
-            let score = max 0s (prevCharScore + gapPenalty)
-            scoreMatrix[0, i+1] <- score
-            prevCharScore <- score
+            H0[off] <-
+                if inGap then
+                    max (prevH0 + Bonus.scoreGapExtension) 0s
+                else
+                    max (prevH0 + Bonus.scoreGapStart) 0s
 
-            consecutiveMatrix[0, i+1] <- 1s
+            C0[off] <- 0s
             inGap <- true
 
-        prevCharClass <- charClass
-        i <- i+1
+        prevH0 <- H0[off] // TODO: Benchmark
 
-    // Prevent character omission
-    if queryCharIdx <> m then
+    if pidx <> M then // Input doesn't contain pattern
         None
     else
 
-    // Phase 2: Compute the scores matrix
-    for i = 1 to m-1 do
-        inGap <- false
-        scoreMatrix[i, 0] <- 0s
+    if M = 1 then
+        if withPos then
+            Some (0, 0, 0s, null) // TODO: output array
+        else
+            Some (0, 0, 0s, null)
+    else
 
-        for j = int firstCharOccurrence[i]+1 to lastMatchingIndex+1 do // Prevent useless computing
-            let diagScore = scoreMatrix[i-1, j-1]
-            let diagConsecutive = consecutiveMatrix[i-1, j-1]
-            let leftScore = scoreMatrix[i, j-1]
+    // Phase 3: Fill in score matrix
+    // Unlike the original algorithm, we do not allow omission.
+    let f0 = F[0]
+    let width = lastIdx - f0 + 1
+    let mutable offset16, H = slab |> Slab.alloc16 offset16 (width*M)
+    // copy(H, H0[f0:lastIdx+1]) // TODO: Consider usage of span
+    for i = f0 to lastIdx do
+        H[i] <- H0[i]
 
+    // Possible length of consecutive chunk at each position.
+    let mutable _, C = slab |> Slab.alloc16 offset16 (width*M)
+    // TODO: Consider usage of span
+    for i = f0 to lastIdx do
+        C[i] <- C0[i]
+
+    let FSub = F.Slice(1, F.Count - 1)
+    let PSub = ArraySegment(pattern, 1, FSub.Count)
+    for off = 0 to FSub.Count - 1 do
+        let f = FSub[off]
+        let pchar = PSub[off]
+        let pidx = off + 1
+        let row = pidx * width
+        let mutable inGap = false
+        let TSub = T.Slice(f, lastIdx+1 - f)
+        let BSub = B.Slice(f).Slice(0, TSub.Count)
+        let mutable CSub = C.Slice(row+f-f0).Slice(0, TSub.Count)
+        let CDiag = C.Slice(row+f-f0-1-width).Slice(0, TSub.Count)
+        let mutable HSub = H.Slice(row+f-f0).Slice(0, TSub.Count)
+        let HDiag = H.Slice(row+f-f0-1-width).Slice(0, TSub.Count)
+        let mutable HLeft = H.Slice(row+f-f0-1).Slice(0, TSub.Count)
+        HLeft[0] <- 0s
+
+        for off = 0 to TSub.Count - 1 do
+            let c = char TSub[off]
+            let col = off + f
             let mutable consecutive = 0s
 
-            // Compute the score if choosing left -> s2
-            let gapPenalty =
-                match inGap with
-                | false -> Bonus.scoreGapStart
-                | true -> Bonus.scoreGapExtension
-            let s2 = leftScore + gapPenalty
+            let s2 =
+                if inGap then
+                    HLeft[off] + Bonus.scoreGapExtension
+                else
+                    HLeft[off] + Bonus.scoreGapStart
 
-            // Compute the score if choosing diagonal -> s1
             let s1 =
-                if query[i] = candidate[j-1] then // Is match
-                    let score = diagScore + Bonus.scoreMatch
-                    let mutable bonus = bonuses[j-1]
-                    consecutive <- diagConsecutive + 1s
-
-                    if consecutive > 1s then // If extending the chunk
-                        /// The positional bonus of the first character of the chunk
-                        let firstBonus = bonuses[j-int diagConsecutive]
-                        if bonus >= Bonus.boundary && bonus > firstBonus then // Break consecutive chunk
+                if pchar = c then
+                    let score = HDiag[off] + Bonus.scoreMatch
+                    let mutable b = BSub[off]
+                    consecutive <- CDiag[off] + 1s
+                    if consecutive > 1s then
+                        let fb = B[col - int consecutive + 1]
+                        // Break consecutive chunk
+                        if b >= Bonus.boundary && b > fb then
                             consecutive <- 1s
                         else
-                            bonus <- Math.Max(bonus, Math.Max(Bonus.consecutive, firstBonus))
+                            b <- max b (max Bonus.consecutive fb)
 
-                    // Choose between starting a new chunk or extending the current
-                    if score + bonus < s2 then
+                    if score+b < s2 then
                         consecutive <- 0s
-                        bonuses[j-1]
+                        score + BSub[off]
                     else
-                        score + bonus
+                        score + b
                 else
                     0s
 
-            // Choose the best score
-            let score = max (max s1 s2) 0s
-            scoreMatrix[i, j] <- score
-            consecutiveMatrix[i, j] <- consecutive
-
+            CSub[off] <- consecutive
             inGap <- s1 < s2
-
-            if score >= bestScore then
-                bestScore <- score
-                bestPos <- i, j
+            let score = max (max s1 s2) 0s
+            if pidx = M-1 && score > maxScore then
+                maxScore <- score
+                maxScorePos <- col
+            HSub[off] <- score
 
     #if DEBUG && !FABLE_COMPILER
-    debug scoreMatrix query candidate
+    debug T pattern F lastIdx H C
     #endif
 
-    // Phase 3: Backtrack
-    let mutable i = fst bestPos
-    let mutable j = snd bestPos
+    // Phase 4: Backtrace to find the character positions
+    let matchedPositions = posSet withPos M
+    let mutable j = f0
+    if withPos then
+        let mutable i = M-1
+        j <- maxScorePos
+        let mutable preferMatch = true
+        let mutable finished = false
 
-    let mutable matchingCharacter =
-        #if FABLE_COMPILER
-        Array.zeroCreate n
-        #else
-        GC.AllocateUninitializedArray<bool> n
-        #endif
+        while not finished do
+            let I = i * width
+            let j0 = j - f0
+            let s = H[I+j0]
 
-    while i >= 0 && j >= 0 do
-        let diagScore = if i = 0 then 0s else scoreMatrix[i, j-1]
-        let leftScore = scoreMatrix[i, j-1]
-        let s = scoreMatrix[i, j]
+            let s1 =
+                if i > 0 && j >= F[i] then
+                    H[I-width+j0-1]
+                else
+                    0s
 
-        if s > leftScore && s > diagScore then
-            matchingCharacter[j-1] <- true
-            i <- i-1
-        j <- j-1
+            let s2 =
+                if j > F[i] then
+                    H[I+j0-1]
+                else
+                    0s
 
-    Some (bestScore, matchingCharacter)
+            if s > s1 && (s > s2 || s = s2 && preferMatch) then
+                j+minIdx |> matchedPositions.Add |> ignore
+                if i = 0 then
+                    j <- j+1 // To cancel the `j <- j-1`
+                    finished <- true
+                i <- i-1
+
+            preferMatch <- C[I+j0] > 1s || I+width+j0+1 < C.Count && C[I+width+j0+1] > 0s
+            j <- j-1
+
+    // Start offset we return here is only relevant when begin tiebreak is used.
+    // However finding the accurate offset requires backtracking, and we don't
+    // want to pay extra cost for the option that has lost its importance.
+    Some (minIdx + j, minIdx + maxScorePos + 1, maxScore, matchedPositions)
